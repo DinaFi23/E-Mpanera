5e2e0cb0e479490b2e2f9a6374f079cfd8a1d3c4
DinaFi23 <rdinafi@gmail.com>
2025-10-14 21:41:39 +0300

    To do list pour initialiser

--- server.js
+++ server.js
@@ -5,14 +5,8 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     // envoye vers clients
-    res.sendFile(path.join(__dirname, 'Public/Account/users/index.html'));
-  })
-
-app.get('/Admin', (req, res) => {
-    // envoye vers Admin
-    res.sendFile(path.join(__dirname, 'Public/Account/Admin/index.html'));
+    res.sendFile(path.join(__dirname, 'Public/index.html'));
 })
-
 
 app.listen(port, () => {
   console.log(`le Serveur est lancé sur le port  ${port}`)
