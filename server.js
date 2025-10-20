da652c5450d44c43c52c266e2c97ae357c71aca7
DinaFi23 <rdinafi@gmail.com>
2025-10-20 20:28:54 +0300

    Connection vers Pg avec Client

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const app = express();
 const port = 3000;
 const dotenv = require('dotenv')
 const pg = require('pg')
 dotenv.config();
+const { Client } = require('pg');
 
-const { Pool } = pg;
-const pool = new Pool({
+const Connex = new Client({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
@@ -15,25 +15,21 @@ const pool = new Pool({
   port: process.env.PGPORT,
 });
 
-
-async function testC() {
-  try { 
-    const res = await
-    pool.query("SELECT NOW()");
-    console.log('Connection reussi');
-    console.log("heure du serveur : ", res.rows[0].now);
-  } catch (error) {
-    console.error("erreur du connection :", error.message);
-  } finally {
-    pool.end();
-  }
-}
-
-testC();
-app.get('/', (req, res) => {
-    // envoye vers clients
-    const query = SELECT * FROM list 
-    res.sendFile(path.join(__dirname, 'Public/index.html'));
+// conncection vers PostgreSql 
+Connex.connect().then(()=>{
+  console.log('Database Connected');
+})
+ 
+//récuperation de donnée 
+app.get('/fetchdata', (req, res) => {
+    const fetch = "Select * from list"
+    Connex.query(fetch,(err, result)=>{
+        if (err) {
+          res.send(err);
+        } else{
+          res.send(result.rows);
+        }
+    })
 })
 
 app.listen(port, () => {
