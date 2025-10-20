87d72f6056472096e4fd2a7c416ef7500bd139af
DinaFi23 <rdinafi@gmail.com>
2025-10-20 19:42:58 +0300

    Connectio to Pg

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const port = 3000;
+const dotenv = require('dotenv')
+const pg = require('pg')
+dotenv.config();
 
+const { Pool } = pg;
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+
+async function testC() {
+  try { 
+    const res = await
+    pool.query("SELECT NOW()");
+    console.log('Connection reussi');
+    console.log("heure du serveur : ", res.rows[0].now);
+  } catch (error) {
+    console.error("erreur du connection :", error.message);
+  } finally {
+    pool.end();
+  }
+}
+
+testC();
 app.get('/', (req, res) => {
     // envoye vers clients
+    const query = SELECT * FROM list 
     res.sendFile(path.join(__dirname, 'Public/index.html'));
 })
 
